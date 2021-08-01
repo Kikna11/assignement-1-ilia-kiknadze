299890e8b7d28f98e82105e1fc8bc404ef058d6b
Kikna11 <88047811+Kikna11@users.noreply.github.com>
2021-08-01 22:04:05 +0400

    Add files via upload

---
+++ davaleba3.java
@@ -0,0 +1,17 @@
+public class davaleba3 {
+    private static int calculate (int a , int b){
+    int gayopa = a / b;
+    return gayopa;
+
+    }
+
+    public static void main(String[] args) {
+        int a = 0;
+        int b = 2;
+        int gayopa;
+        gayopa = calculate(a , b);
+        System.out.println(gayopa);
+
+    }
+
+}
